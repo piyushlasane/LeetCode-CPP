ad2cc03b5add7b69502453a3f19b3e5185a3e9c0
piyushlasane <piyushlasane@gmail.com>
2024-06-23 23:46:04 +0530

    commit from git

---
+++ RemoveDuplicates.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        auto it = unique(nums.begin(), nums.end());
+        nums.erase(it, nums.end());
+        return nums.size();
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    Solution solution;
+    int newSize = solution.removeDuplicates(nums);
+
+    cout << "New size after removing duplicates: " << newSize << endl;
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < newSize; ++i) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}

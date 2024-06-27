0691e539a3342379bed1c3841b4e6fba01a3912e
piyushlasane <piyushlasane@gmail.com>
2024-06-27 21:17:32 +0530

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	new file:   IsValid.cpp

---
+++ IsValid.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        string stk;
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                stk.push_back(c);
+            } else if (stk.empty() || !match(stk.back(), c)) {
+                return false;
+            } else {
+                stk.pop_back();
+            }
+        }
+        return stk.empty();
+    }
+
+private:
+    bool match(char l, char r) {
+        return (l == '(' && r == ')' || l == '[' && r == ']' || l == '{' && r == '}');
+    }
+};
+
+int main() {
+    string input;
+    cout << "Enter a string containing brackets: ";
+    cin >> input;
+
+    Solution solution;
+    bool result = solution.isValid(input);
+
+    cout << "The string is " << (result ? "valid" : "invalid") << endl;
+
+    return 0;
+}

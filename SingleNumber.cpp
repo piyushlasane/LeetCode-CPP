b723c774e64e0759fb3a64d3dbaea178502166c1
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-07-11 23:35:09 +0530

    Create SingleNumber.cpp

---
+++ SingleNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution solution;
+    int result = solution.singleNumber(nums);
+
+    cout << "The single number is: " << result << endl;
+
+    return 0;
+}
